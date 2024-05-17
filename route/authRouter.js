de65b5a0defe17c83ceb635bedee98dfcc757c6d
Abdur-Rehman-Malik <abdurrehmanm849@gmail.com>
2024-05-18 01:02:31 +0500

    save changes

--- route/authRouter.js
+++ route/authRouter.js
@@ -1,8 +1,7 @@
 const route = require("express").Router();
 const authrouter = require("../controller/authControllers");
-const userController = require("../controller/userController");
 
 route.get("/login", authrouter.login);
 route.get("/logout", authrouter.logout);
-route.post("createUser", userController.createUser);
+
 module.exports = route;
